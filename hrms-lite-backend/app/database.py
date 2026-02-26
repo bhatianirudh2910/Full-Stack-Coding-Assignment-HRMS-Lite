903c8e8efd6b696e18b6de73d7e84bddf03610b8
Anirudh Bhati <bhatianirudh2910@gmail.com>
2026-02-26 16:51:19 +0530

    chnages

--- hrms-lite-backend/app/database.py
+++ hrms-lite-backend/app/database.py
@@ -4,19 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not MONGO_URL:
+if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL environment variable not set")
 
 client = AsyncIOMotorClient(
-    MONGO_URL,
-    tls=True,
+    DATABASE_URL,
     serverSelectionTimeoutMS=30000,
     connectTimeoutMS=30000,
 )
 
-database = client.hrms_lite
+database = client["hrms_lite"]
 
-employee_collection = database.get_collection("employees")
-attendance_collection = database.get_collection("attendance")
+employee_collection = database["employees"]
+attendance_collection = database["attendance"]
